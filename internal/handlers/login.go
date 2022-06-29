7678c4f148bbacfb3cd5e84a137ed10c24ca3408
Vadim Nikolaev <starlei165@gmail.com>
2022-06-29 11:52:01 +0300

    feat: logging in account

---
+++ internal/handlers/login.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nikolaevv/my-investor/internal/handlers/requests"
+)
+
+func (h *Handler) Login(c *gin.Context) {
+	var req requests.UserAuth
+	if err := c.BindJSON(&req); err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("error: %s", err))
+		return
+	}
+	if err := req.Validate(); err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("error: %s", err))
+		return
+	}
+
+	user, err := h.Repo.User.GetUserByLogin(req.Login)
+	if err != nil {
+		c.String(http.StatusNotFound, fmt.Sprintf("error: %s", err))
+		return
+	}
+
+	if err = h.Hasher.CheckPassword(req.Password, user.PasswordHash); err != nil {
+		c.String(http.StatusForbidden, fmt.Sprintf("error: %s", err))
+		return
+	}
+
+	tokens, err := CreateUserSession(
+		user.ID,
+		h.Repo,
+		h.Auth,
+		h.Config,
+	)
+	if err != nil {
+		c.String(http.StatusBadGateway, fmt.Sprintf("error: %s", err))
+		return
+	}
+
+	c.JSON(200, tokens)
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
+	ID           uint
 	Login        string `gorm:"not null,unique"`
 	PasswordHash string `json:"-" gorm:"not null"`
 	RefreshToken string `json:"-"`
